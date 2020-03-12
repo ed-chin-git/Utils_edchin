d1c491aa704c49f2fc572677fafedb53c0263d7d
ed.chin.git <edgardochin@gmail.com>
2020-03-12 16:46:08 -0500

    test empty __init__.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open('README.md','r') as fh:
 
 setuptools.setup(
     name='utils-edchin',
-    version='1.0.0' ,
+    version='1.0.1' ,
     author='Ed Chin',
     description="Personal Data Science Utilities in Python",
     long_description=LONG_DESCRIPTION,
