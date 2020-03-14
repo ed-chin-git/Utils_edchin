93148a1b3386bbd84b8705391547b35d95e304d4
ed.chin.git <edgardochin@gmail.com>
2020-03-14 13:25:17 -0500

    New Version to pypi

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open('README.md','r') as fh:
 # Go to "New and Changed setup() Keywords"
 setuptools.setup(
     name='utils-edchin',
-    version='1.0.7',
+    version='1.0.8',
     author='Ed Chin',
     description="Personal Data Science Utilities in Python",
     long_description=LONG_DESCRIPTION,
