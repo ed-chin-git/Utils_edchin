1c713981b469767a35101488e09b87769bd5784f
ed.chin.git <edgardochin@gmail.com>
2020-03-19 12:23:51 -0500

    Update __init__py docs

--- utils_edchin/__init__.py
+++ utils_edchin/__init__.py
@@ -1,7 +1,13 @@
 '''
 https://medium.com/@joel.barmettler/how-to-upload-your-python-package-to-pypi-65edc5fe9c56
 
-The __init__.py file is used to mark which classes you want the user to access through the package interface.
-     from Package.ClassFile import *
+The presence of __init__.py file identifies the folder as a Python Package
+And is used to mark which classes you want the user to access through the package interface.
+  i.e  from Package.ClassFile import *
+
+In this case:
+     from utils_edchin.Dataprocessor import *
+
+
 '''
 from utils_edchin.DataProcessor import *
