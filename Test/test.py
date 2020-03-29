1225e029982a7415ad961c95fd8081b78152b89d
ed.chin.git <edgardochin@gmail.com>
2020-03-29 07:10:08 -0500

    Update to pypi

--- Test/test.py
+++ Test/test.py
@@ -22,8 +22,9 @@ if __name__ == "__main__":
     print (dir(dp))
     print (dir(pyx))
     
-    df_out = dp.add_state_names(dp, df_in)
+    df_out = dp.add_state_names(df_in)
     print('Output:\n', df_out.head()) 
 
-    print(pyx.variance_edc(pyx, [5, 25, 99, 1325, 1125, 555, 6546, 888]))
-    print(pyx.removeOutliers(pyx, [-10, 2, 5, 3, 8, 4, 7, 5, 10, 99, 1000]))
+    print(pyx.variance_edc([5, 25, 99, 1325, 1125, 555, 6546, 888]))
+    print(pyx.removeOutliers([-10, 2, 5, 3, 8, 4, 7, 5, 10, 99, 1000]))
+
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ import setuptools
 
 REQUIRED = [
     'numpy',
-    'pandas',utils
+    'pandas',
     'sklearn'
     ]
 
@@ -28,7 +28,7 @@ with open('README.md','r') as fh:
 # Go to "New and Changed setup() Keywords "
 setuptools.setup(
     name='utils-edchin',
-    version='1.0.18',
+    version='1.0.19',
     author='Ed Chin',
     description="Personal Data Science Utilities in Python",
     long_description=LONG_DESCRIPTION,
